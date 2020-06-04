daabacd3ac6fba2ed6c60fb1aaa94ec8f587c61d
akosgarai <szlem01@gmail.com>
2020-06-04 16:28:10 +0200

    Point vs AABB

--- coldet.go
+++ coldet.go
@@ -8,6 +8,25 @@ type AABB struct {
 	height   float32 // Y axis
 }
 
+type Point struct {
+	position [3]float32
+}
+
+// X returns the x component of the position.
+func (p *Point) X() float32 {
+	return p.position[0]
+}
+
+// Y returns the y component of the position.
+func (p *Point) Y() float32 {
+	return p.position[1]
+}
+
+// Z returns the z component of the position.
+func (p *Point) Z() float32 {
+	return p.position[2]
+}
+
 // X returns the x component of the position.
 func (a *AABB) X() float32 {
 	return a.position[0]
@@ -18,7 +37,7 @@ func (a *AABB) Y() float32 {
 	return a.position[1]
 }
 
-// Z returns the z component of the position/
+// Z returns the z component of the position.
 func (a *AABB) Z() float32 {
 	return a.position[2]
 }
@@ -45,4 +64,13 @@ func CheckAabbVsAabb(b1, b2 AABB) bool {
 	colZ := b1.Z()+b1.Length()/2 >= b2.Z()-b2.Length()/2 && b2.Z()+b2.Length()/2 >= b1.Z()-b1.Length()/2
 
 	return colX && colY && colZ
+}
+
+// CheckPointVsAabb returns true if the given point is inside the AABB.
+func CheckPointVsAabb(p Point, b AABB) bool {
+	inX := p.X() > b.X()-b.Width()/2 && p.X() < b.Width()/2
+	inY := p.Y() > b.Y()-b.Height()/2 && p.Z() < b.Height()/2
+	inZ := p.Z() > b.Z()-b.Length()/2 && p.Z() < b.Length()/2
+
+	return inX && inY && inZ
 }
--- coldet_test.go
+++ coldet_test.go
@@ -6,34 +6,50 @@ import (
 
 var (
 	aabb = AABB{[3]float32{1, 2, 3}, 4, 5, 6}
+	p    = Point{[3]float32{1, 2, 3}}
 )
 
-func TestX(t *testing.T) {
+func TestPointX(t *testing.T) {
+	if p.X() != 1 {
+		t.Error("Invalid x coordinate.")
+	}
+}
+func TestPointY(t *testing.T) {
+	if p.Y() != 2 {
+		t.Error("Invalid y coordinate.")
+	}
+}
+func TestPointZ(t *testing.T) {
+	if p.Z() != 3 {
+		t.Error("Invalid z coordinate.")
+	}
+}
+func TestAABBX(t *testing.T) {
 	if aabb.X() != 1 {
 		t.Error("Invalid x coordinate.")
 	}
 }
-func TestY(t *testing.T) {
+func TestAABBY(t *testing.T) {
 	if aabb.Y() != 2 {
 		t.Error("Invalid y coordinate.")
 	}
 }
-func TestZ(t *testing.T) {
+func TestAABBZ(t *testing.T) {
 	if aabb.Z() != 3 {
 		t.Error("Invalid z coordinate.")
 	}
 }
-func TestWidth(t *testing.T) {
+func TestAABBWidth(t *testing.T) {
 	if aabb.Width() != 4 {
 		t.Error("Invalid width.")
 	}
 }
-func TestLength(t *testing.T) {
+func TestAABBLength(t *testing.T) {
 	if aabb.Length() != 5 {
 		t.Error("Invalid length.")
 	}
 }
-func TestHeight(t *testing.T) {
+func TestAABBHeight(t *testing.T) {
 	if aabb.Height() != 6 {
 		t.Error("Invalid heigth.")
 	}
@@ -55,6 +71,26 @@ func TestCheckAabbVsAabb(t *testing.T) {
 	for _, tt := range testData {
 		result := CheckAabbVsAabb(tt.b1, tt.b2)
 		if result != tt.collided {
+			t.Error("Invalid collision result.")
+		}
+	}
+}
+func TestCheckPointVsAabb(t *testing.T) {
+	testData := []struct {
+		p  Point
+		b  AABB
+		in bool
+	}{
+		{Point{[3]float32{0, 0, 0}}, AABB{[3]float32{0, 0, 0}, 1, 1, 1}, true},
+		{Point{[3]float32{0, 0, 0}}, AABB{[3]float32{1, 1, 1}, 1, 1, 1}, false},
+		{Point{[3]float32{0, 0, 0}}, AABB{[3]float32{1, 1, 1}, 2, 2, 2}, false},
+		{Point{[3]float32{0.5, 0.5, 0.5}}, AABB{[3]float32{0, 0, 0}, 1, 1, 1}, false},
+		{Point{[3]float32{0.49, 0.49, 0.5}}, AABB{[3]float32{0, 0, 0}, 1, 1, 1}, false},
+		{Point{[3]float32{0.49, 0.49, 0.49}}, AABB{[3]float32{0, 0, 0}, 1, 1, 1}, true},
+	}
+	for _, tt := range testData {
+		result := CheckPointVsAabb(tt.p, tt.b)
+		if result != tt.in {
 			t.Error("Invalid collision result.")
 		}
 	}
